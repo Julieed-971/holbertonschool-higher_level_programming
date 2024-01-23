b9d088dce4c2c27cb239219e8ef6696eca0c09fb
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-23 10:34:46 +0100

    Fixed pycodestyle

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i == 99:
         print("{:02d}".format(i))
     elif i >= 0 and i <= 98:
-    	print("{:02d}, ".format(i), end='')
+        print("{:02d}, ".format(i), end='')
