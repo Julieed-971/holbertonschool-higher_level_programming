8144ec5b315399b79ea39d0039c2710bd0d0dca4
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-26 12:04:28 +0100

    Fixed pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0 and i % 3 != 0:
             print("Buzz ", end="")
         else:
-            print("{} ".format(i), end = "")
-            
+            print("{} ".format(i), end="")
