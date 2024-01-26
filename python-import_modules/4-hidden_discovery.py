20e1435a1618d9cffba0fdb5fb26274b5fd15382
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-26 16:13:04 +0100

    Program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if not name[0] == '_'and not name[1] == '_':
+            print("{}".format(name))
