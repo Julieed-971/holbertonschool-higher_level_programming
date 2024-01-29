e0291f0ab1d3fa9b5eaa66091ecd5c1664679831
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-29 12:26:00 +0100

    Removed for loop

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    for i in range(len(my_list)):
-        if idx < 0:
+        if idx < 0 or idx >= len(my_list):
             return None
-        elif idx > len(my_list):
-            return None
-        elif i == idx:
-            return my_list[i]
+        return my_list[idx]
