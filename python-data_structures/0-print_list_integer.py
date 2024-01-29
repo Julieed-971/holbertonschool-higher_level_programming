f259fe67aa4a06d16ead9ecadd95b3a3d45d313f
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-29 11:23:34 +0100

    Fixed loop

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{}".format(item))
+    for i in range(len(my_list)):
+        print("{i}".format(my_list[i]))
