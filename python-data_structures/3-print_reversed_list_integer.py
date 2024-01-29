75a6c554ca0c18e1c631cba91a396498dc8f7f26
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-29 13:17:54 +0100

    Fixed specifier in print

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
