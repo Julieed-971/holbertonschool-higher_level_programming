5510f8d2ca3572d62370f0bbc6d6ecd8b6bd599b
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-29 18:02:56 +0100

    Replaced join to a for loop

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print("{:s}".format(' '.join(map(str, row))))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
+
