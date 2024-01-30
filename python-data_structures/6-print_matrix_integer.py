0941806b5de46d805eab38bd6bd87477118db7f2
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-30 08:03:31 +0100

    Added spaces between integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end='')
+            if j == len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end='')
+            else:
+                print("{:d} ".format(matrix[i][j]), end='')
         print()
