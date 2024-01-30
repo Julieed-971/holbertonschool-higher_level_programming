e74f8d9ed95f2856c27283ee6ab93374e4d11680
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-30 10:35:03 +0100

    Prevented more than 2 elements to be added

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -14,5 +14,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_c = 0, 0
         add = tuple(map(lambda x, y: x + y, tuple_a, tuple_c))
     else:
-        add = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+        add = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return add
