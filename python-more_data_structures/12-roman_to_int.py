f0597bc1fd91699adb1b3d82bd73e9d500e37fc0
Julieed-971 <julie.dedieu1@gmail.com>
2024-01-31 22:17:37 +0100

    Function that convert roman numeral to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string and type(roman_string) == str:
+        convert_dict = {'I': 1,
+                        'V': 5,
+                        'X': 10,
+                        'L': 50,
+                        'C': 100,
+                        'D': 500,
+                        'M': 1000}
+        convert_list = []
+
+        for i in range(len(roman_string)):
+            current_value = convert_dict[roman_string[i]]
+
+            if i > 0 and convert_dict[roman_string[i-1]] < current_value:
+                convert_list[-1] *= -1
+            convert_list.append(current_value)
+
+        result = sum(convert_list)
+        return result
+    return 0
