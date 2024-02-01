22733982883ad3b27baf292c9538e2074d9ece7f
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-01 20:24:34 +0100

    Resolved exception result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,6 +5,7 @@ def safe_print_division(a, b):
     try:
         div_result = a / b
     except ZeroDivisionError:
+        div_result = None
         return None
     finally:
         print("Inside result: {}".format(div_result))
