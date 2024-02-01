134eb3dd2dc1bf26636dbb6e121d2850c5864de4
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-01 13:51:02 +0100

    Fixed pycodestyle

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")            
+            print(my_list[i], end="")         
             count += 1
         except IndexError:
             break
