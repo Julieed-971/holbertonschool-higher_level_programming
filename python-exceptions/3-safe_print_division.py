379c8cf67bb54ae22c27fe76af234a8cc54a42c1
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-01 20:09:09 +0100

    Function that divides 2 integers and prints the result.

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    div_result = 0
+    try:
+        div_result = a / b
+        return div_result
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {:.1f}".format(div_result))
+	
