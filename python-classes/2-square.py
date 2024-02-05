46805ff885b9c080801768ecb61150eee6d306f2
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-05 22:03:22 +0100

    Applied Numpydoc style

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,10 +7,23 @@ class Square:
     pass
 
     def __init__(self, size=0):
+        """Initialize the size and check if size is an integer
+
+        Parameters
+        ----------
+        size : int, optional
+        Size of the square, optional
+        Default=0
+
+        Raises
+        ------
+        TypeError
+            If the size is not of integer type
+        ValueError
+            If size is strictly inferior to zero
+
         """
-        This function initialize the size
-        and check if size is an integer
-        """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
