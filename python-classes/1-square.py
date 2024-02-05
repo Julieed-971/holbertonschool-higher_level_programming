f9d03b2c5c138cb194291fb8a627ed135c7c4b68
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-05 15:57:04 +0100

    Script that creates an empty class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Second task - defines an empty square"""
+
+
+class Square:
+    """This is an empty class that defines a square."""
+    pass
+
+    def __init__(self, size):
+        """This function initialize the size"""
+        self.__size = size
