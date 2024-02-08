98552f0fd963f9aa7fe2a8b5fcb6bae9b229fd7a
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-08 15:24:14 +0100

    Trying other syntax

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,9 +16,9 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if type(a) is float:
+    if isinstance(a, float):
         a = int(a)
-    if type(b) is float:
+    if isinstance(b, float):
         b = int(b)
     sum = a + b
     return sum
