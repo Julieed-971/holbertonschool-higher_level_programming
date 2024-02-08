4372820c9240c6712908330389fb1334ecbc3660
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-08 15:38:54 +0100

    Fixed integer casting

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,9 +16,5 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
     sum = a + b
-    return sum
+    return int(sum)
