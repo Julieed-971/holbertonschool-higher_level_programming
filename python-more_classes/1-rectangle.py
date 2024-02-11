9e43f224977fe2d634fc705b5ba48f213ec87125
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-11 09:53:06 +0100

    Script that get and set the width and height of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -83,7 +83,7 @@ class Rectangle:
         """
 
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
