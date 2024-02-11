ef4261f79cc0a0ffb7918b23068f765606126a7e
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-11 15:32:23 +0100

    Added script to compare two rectangle areas

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -137,9 +137,6 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if rect_2.area() > rect_1.area():
-            return rect_2
-        if rect_2.area() == rect_1.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_1
+        return rect_2
