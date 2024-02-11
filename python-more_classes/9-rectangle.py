adbd020947e1f258125fcd4242f7f3d523f75656
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-11 16:22:30 +0100

    Added script to create a square through class method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -7,6 +7,10 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+
     def __init__(self, width=0, height=0):
         """Initialize the rectangle width and height
 
@@ -140,7 +144,3 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
