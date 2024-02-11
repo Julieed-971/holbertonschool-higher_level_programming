175cc27cd35336c81050db574ec5c1b860164b1d
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-11 17:02:06 +0100

    Added script to create a square through class method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -23,7 +23,6 @@ class Rectangle:
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
-        Rectangle.print_symbol = self.print_symbol
 
     @property
     def width(self):
