453ac3d12561da35cc0bb9843d60c1cc2e7910a0
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-13 12:01:25 +0100

    Checks if the object is an instance of a class that inherited (directly or indirectly) of the specified class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Checks if the object is an instance of a class that inherited
+(directly or indirectly) of the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if the object is an instance of a class that inherited
+(directly or indirectly) of the specified class"""
+    if issubclass(type(obj), a_class) and a_class != bool:
+        return True
+    return False
