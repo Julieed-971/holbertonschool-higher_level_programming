c509a4580dc38427156ab1ee9d17961ae5bff09a
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-13 07:58:50 +0100

    Class that sorts a list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""MyList Class."""
+
+
+class MyList(list):
+    """Empty class."""
+    
+    pass
+
+    def print_sorted(self):
+        """Sorts list."""
+        print(sorted(self))
