318f0d4a0ff27f8055a6bf1bcada824d7c14366b
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-13 13:13:55 +0100

    Creates BaseGeometry class

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write an empty geometry class."""
+
+
+class BaseGeometry:
+    """Empty geometry class."""
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
