d084b62decda29b29c5f1fcd8d725319466148c4
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-15 00:11:09 +0100

    Serialize attributes of a student class to a dictionnary

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """Student class."""
+    pass
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student class."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Serialize the attributes of a class to a dictionnary."""
+        if attrs is None:
+            return vars(self)
+        elif isinstance(attrs, list) and\
+                all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
