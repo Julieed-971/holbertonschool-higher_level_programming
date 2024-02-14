e6de3e3279d1702fb8f65fd8d3b2ea0741f974ee
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-14 13:28:55 +0100

    Function that writes a string in a text file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Write a string to a text file (UTF8) and 
-returns the number of characters written"""
+"""Write a string to a text file (UTF8) and
+returns the number of characters written."""
 
 
 def write_file(filename="", text=""):
