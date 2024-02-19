82f51fbffbd97910aa7b820c1d6151a0bfa9bdaa
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-19 23:11:37 +0100

    Added function that returns the JSON representation of list_dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class for the full project"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Return the JSON representation of list_dictionaries."""
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
