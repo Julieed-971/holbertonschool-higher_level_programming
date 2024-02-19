c0cc27653fc2d3bc8a41936321109c28a6e00478
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-19 12:28:30 +0100

    Added Rectangle description

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,8 @@ class Rectangle(Base):
             for _ in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """Return the rectangle description."""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                self.__y, self.__width, self.__height))
