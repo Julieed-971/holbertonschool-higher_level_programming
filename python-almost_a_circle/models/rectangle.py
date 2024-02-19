f1d1d546a98e2b542d4799092f9965f9a3e93d68
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-19 14:07:31 +0100

    Added function that updates Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,18 @@ class Rectangle(Base):
         """Return the rectangle description."""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                 self.__y, self.__width, self.__height))
+
+    def update(self, *args):
+        """Assign an argument to each attribute"""
+
+        for _ in args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
