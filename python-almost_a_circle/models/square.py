c97a491f29076833c3f2f3b365d2a6dbc7c60e1a
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-19 22:00:18 +0100

    Added getter and setter for square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -27,5 +27,4 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
