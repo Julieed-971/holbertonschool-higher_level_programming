eea66a5edbf63e5b8e8a854655839c9290bcb9c4
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-20 15:35:51 +0100

    Unittests file

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,14 +7,6 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_id(self):
-        # Create multiple instances of Base
+        # Check if ID is greater than 0
         base = Base()
-        
-        # Check if instance has an ID attribute
-        self.assertTrue(hasattr(base, 'id'))
-
-        # Check if ID is an integer
-        self.assertIsInstance(base.id, int)
-        
-        # Check if ID is greater than or equal to 1
-        self.assertGreaterEqual(base.id, 1)
+        self.assertGreater(base.id, 0)
