4db07bcf63c158b74a0a6c18b3d2f01e201a6bd8
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-21 16:33:16 +0100

    Unittest for rectangle class

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -118,19 +118,6 @@ class TestRectangle(unittest.TestCase):
         expected_output = []
         self.assertEqual(dict_output, expected_output)
     
-    def test_save_to_file_empty_list_file_content(self):
-        filename = "Rectangle.json"
-
-        Rectangle.save_to_file([])
-        self.assertTrue(os.path.exists(filename))
-        
-        with open(filename, "r") as f:
-            file_content = f.read()
-
-        self.assertEqual(file_content, "[]")
-        
-        os.remove(filename)
-    
     def test_save_to_file_with_list_of_rectangle(self):
         filename = "Rectangle.json"
         rectangle = Rectangle(1, 2)
