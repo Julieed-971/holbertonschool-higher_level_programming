bef45b370c33de69dbb4ef95cff05a2811a8e34a
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-21 17:08:20 +0100

    Fixed save_to_file

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -104,7 +104,7 @@ class TestRectangle(unittest.TestCase):
         Rectangle.save_to_file([])
         
         with open("Rectangle.json", "r") as f:
-            self.assertEqual(f.read(), "[]")
+            self.assertEqual(f.read(), [])
 
     def test_save_to_file_empty_list(self):
         filename = "Rectangle.json"
