02f62836e2e74f585d7b6cb254c0f04736001314
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-21 14:22:05 +0100

    Unittest for rectangle class

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -100,6 +100,12 @@ class TestRectangle(unittest.TestCase):
         rectangle = Rectangle.create(**{'id': 89, 'width': 1})
         self.assertTrue(hasattr(rectangle, 'create'))
         
+    def test_save_to_file_exists(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        self.assertTrue(hasattr(r1, 'save_to_file'))
+        
 # Test of update() in Rectangle exists
 
 # Test of update(89) in Rectangle exists
