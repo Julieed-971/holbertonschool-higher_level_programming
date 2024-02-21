a34fc7b7f6b66d432ab3b46fff7e564ab1c7a269
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-21 15:58:28 +0100

    Unittest for rectangle class

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -155,45 +155,6 @@ class TestRectangle(unittest.TestCase):
         dict_output = Rectangle.load_from_file()
         expected_output = [Rectangle(1, 2)]
         self.assertNotEqual(str(dict_output), str(expected_output))
-        
-# Test of update() in Rectangle exists
 
-# Test of update(89) in Rectangle exists
-
-# Test of update(89, 1) in Rectangle exists
-
-# Test of update(89, 1, 2) in Rectangle exists
-
-# Test of update(89, 1, 2, 3) in Rectangle exists
-
-# Test of update(89, 1, 2, 3, 4) in Rectangle exists
-
-# Test of update(**{ 'id': 89 }) in Rectangle exists
-
-# Test of update(**{ 'id': 89, 'width': 1 }) in Rectangle exists
-
-# Test of update(**{ 'id': 89, 'width': 1, 'height': 2 }) in Rectangle exists
-
-# Test of update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 }) in Rectangle exists
-
-# Test of update(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 }) in Rectangle exists
-
-# Test of Rectangle.create(**{ 'id': 89 }) in Rectangle exists
-
-# Test of Rectangle.create(**{ 'id': 89, 'width': 1 }) in Rectangle exists
-
-# Test of Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2 }) in Rectangle exists
-
-# Test of Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3 }) in Rectangle exists
-
-# Test of Rectangle.create(**{ 'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4 }) in Rectangle exists
-
-# Test of Rectangle.save_to_file(None) in Rectangle exists
-
-# Test of Rectangle.save_to_file([]) in Rectangle exists
-
-# Test of Rectangle.save_to_file([Rectangle(1, 2)]) in Rectangle exists
-
-# Test of Rectangle.load_from_file() when file doesn’t exist exists
-
-# Test of Rectangle.load_from_file() when file exists exists
+if __name__ == "__main__":
+    unittest.main()
