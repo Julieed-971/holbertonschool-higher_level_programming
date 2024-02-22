3eef249c7763d3230c7ce23b998dd7006ea65a6e
Julieed-971 <julie.dedieu1@gmail.com>
2024-02-22 09:37:28 +0100

    Added docstring

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -53,14 +53,17 @@ class TestRectangle(unittest.TestCase):
             Rectangle(1, 2, 3, -4)
 
     def test_area(self):
+        """Test the value of the area"""
         rectangle = Rectangle(3, 4)
         self.assertEqual(rectangle.area(), 12)
 
     def test_string_representation(self):
+        """Test the string representation of a rectangle"""
         rectangle = Rectangle(3, 4, 1, 2, 5)
         self.assertEqual(str(rectangle), "[Rectangle] (5) 1/2 - 3/4")
 
     def test_display(self):
+        """Test the output when diplaying a rectangle"""
         rectangle = Rectangle(2, 3)
         actual_output = io.StringIO()
         sys.stdout = actual_output
@@ -80,10 +83,12 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(printed_output, expected_output)
 
     def test_to_dictionary(self):
+        """Test the dictionary representation of a rectangle"""
         rectangle = Rectangle(3, 4, 1, 2, 5)
         self.assertEqual(rectangle.to_dictionary(), {'x': 1, 'y': 2, 'id': 5, 'width': 3, 'height': 4})
 
     def test_update(self):
+        """Test when updating the value of a rectangle"""
         rectangle = Rectangle(1, 2)
         rectangle.update(2, 3, 4, 5, 6)
         self.assertEqual(rectangle.id, 2)
@@ -93,65 +98,22 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle.y, 6)
 
     def test_update_method_exists(self):
+        """Test that the class possess an update method"""
         rectangle = Rectangle(1, 2)
         self.assertTrue(hasattr(rectangle, 'update'))
 
     def test_create_method_exists(self):
+        """Test that the class possess a create method"""
         rectangle = Rectangle.create(**{'id': 89, 'width': 1})
         self.assertTrue(hasattr(rectangle, 'create'))
 
     def test_save_to_file_with_none(self):
+        """Test if the save_to_file method works with a None argument"""
         Rectangle.save_to_file(None)
-        
         with open("Rectangle.json", "r") as f:
             dict_output = json.load(f)
-        
         expected_output = []
         self.assertEqual(dict_output, expected_output)
-
-    def test_save_to_file_empty_list(self):
-        Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as f:
-            self.assertTrue("[]", f.read())
-            
-    def test_save_to_file_empty_list_again(self):
-        self.assertEqual(Rectangle.save_to_file([]), None)
-
-    def test_save_to_file_empty_list_again_and_again(self):
-        filename = "Rectangle.json"
-        Rectangle.save_to_file([])
-        self.assertTrue(os.path.exists(filename))
-        with open(filename, "r") as f:
-            file_content = f.read()
-            self.assertEqual(file_content, '[]')
-        os.remove(filename)  
-    
-    def test_save_to_file_with_list_of_rectangle(self):
-        filename = "Rectangle.json"
-        rectangle = Rectangle(1, 2)
-        Rectangle.save_to_file([rectangle])
-        loaded_rectangles = Rectangle.load_from_file()
-        self.assertEqual(len(loaded_rectangles), 1)
-        loaded_rectangle = loaded_rectangles[0]
-        self.assertEqual(loaded_rectangle.width, rectangle.width)
-        self.assertEqual(loaded_rectangle.height, rectangle.height)
-        self.assertEqual(loaded_rectangle.x, rectangle.x)
-        self.assertEqual(loaded_rectangle.y, rectangle.y)
-        self.assertEqual(loaded_rectangle.id, rectangle.id)
-
-    def test_load_from_file_file_does_not_exist(self):
-        self.assertEqual(Rectangle.load_from_file(), [])
-
-    def test_load_from_file_file_exists(self):
-        Rectangle.save_to_file([Rectangle(1, 2)])
-        dict_output = Rectangle.load_from_file()
-        expected_output = [Rectangle(1, 2)]
-        self.assertNotEqual(str(dict_output), str(expected_output))
-
-    def test_save_to_file_empty_list(self):
-        Rectangle.save_to_file([])
-        with open("Rectangle.json", "r") as f:
-            self.assertTrue("[]", f.read())
 
 if __name__ == "__main__":
     unittest.main()
