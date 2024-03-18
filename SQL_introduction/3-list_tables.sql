4b72a3ea582f3f50984b6ba793bd955ca95f8efd
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-18 16:28:00 +0100

    Added missing semi-colon

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- Script that lists all the tables of a database in MySQL server
-SHOW TABLES
+SHOW TABLES;
---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Script that creates a table called first_table in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, `name` VARCHAR(256));
