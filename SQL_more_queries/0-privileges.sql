55bc2c39bf77e5827cfa43ef49597816e368d3db
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-19 16:16:33 +0100

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- Script that lists all privileges of the MySQL users user_0d_1 on your server (in localhost)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Script that lists all privileges of the MySQL user user_0d_2 on your server (in localhost)
 SHOW GRANTS FOR 'user_0d_2'@'local_host';
