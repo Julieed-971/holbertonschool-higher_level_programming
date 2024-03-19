cc139165331946411729ee9f2acd28067c04a85c
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-19 18:59:51 +0100

    Cities by states

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM cities
+	JOIN states on states.id = cities.state_id
+	ORDER BY cities.id ASC;
