05eef660dfaa9d0f0074da36d25bc0056b657211
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-21 10:06:15 +0100

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+	username = sys.argv[1]
+	password = sys.argv[2]
+	db_name = sys.argv[3]
+
+	# Open database
+	db = MySQLdb.connect("localhost", port=3306, user=username, passwd=password, db=db_name)
+
+	# Create a cursor
+	cursor = db.cursor()
+
+	# Execute SQL query
+	cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+ 
+	# Print results in tuple format
+	for row in cursor.fetchall():
+		print(row)
+ 
+	# Close cursors
+	cursor.close()
+	# Close connection to the database
+	db.close()
