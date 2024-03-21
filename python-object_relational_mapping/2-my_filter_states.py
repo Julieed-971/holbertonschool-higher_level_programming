dcd68213669af3176ccc0777d738d02b670699b0
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-21 11:23:41 +0100

    Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""List all values in states where name matches the argument"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name_srch = sys.argv[4]
+
+# Open database
+    db = MySQLdb.connect("localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create a cursor
+    cursor = db.cursor()
+
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states WHERE states.name\
+                   = '{}' ORDER BY states.id ASC".format(state_name_srch))
+
+    # Print results in tuple format
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close cursors
+    cursor.close()
+
+    # Close connection to the database
+    db.close()
