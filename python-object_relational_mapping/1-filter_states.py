d05e74eab2d64b934a97f68402898b21b568cf9e
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-21 10:19:24 +0100

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""List all states with name starting with N from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Open database
+    db = MySQLdb.connect("localhost", port=3306,
+                         user=username, passwd=password, db=db_name)
+
+    # Create a cursor
+    cursor = db.cursor()
+
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    # Print results in tuple format
+    for row in cursor.fetchall():
+        print(row)
+
+    # Close cursors
+    cursor.close()
+    # Close connection to the database
+    db.close()
