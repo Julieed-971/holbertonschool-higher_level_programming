63802c3949bfe536692ec08e746b34bade4ab79c
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-21 11:48:32 +0100

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
 
     # Execute SQL query
     cursor.execute("SELECT * FROM states WHERE states.name\
-                   = '{}' ORDER BY states.id ASC".format(state_name_srch))
+                   LIKE BINARY '{}' ORDER BY states.id ASC"
+                   .format(state_name_srch))
 
     # Print results in tuple format
     for row in cursor.fetchall():
