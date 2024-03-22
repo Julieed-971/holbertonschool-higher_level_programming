99b0a9ff513ffd287097706ac147e12af7d21cf3
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-22 16:02:08 +0100

    Added case sensitivity

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""List all State object from database hbtn_0e_6_usa"""
-
+"""List State objects from the database hbtn_0e_6_usa where name contains 'a' (case-sensitive)"""
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine, asc, func
 from sqlalchemy.orm import sessionmaker
 
 
@@ -16,8 +15,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_with_a = session.query(State).filter(
-        State.name.like('%a')).order_by(asc(State.id)).all()
+    state_with_a = session.query(State).filter(func.binary(
+        State.name).like('%a')).order_by(asc(State.id)).all()
 
     for state in state_with_a:
         print("{}: {}".format(state.id, state.name))
